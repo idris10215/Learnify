25997764762834d7ce8da09021098eb0258ef1f9
idris10215 <mohammedidris10215@gmail.com>
2025-11-11 10:59:51 +0530

    Updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,16 +24,6 @@ import StudentClassViewPage from "./pages/StudentClassViewPage";
 import StudentModuleViewPage from "./pages/StudentModuleViewPage";
 import ClassAnalyticsReport from "./pages/ClassAnalyticsReport";
 
-// Amplify.configure({
-//   Auth: {
-//     Cognito: {
-//       region: import.meta.env.VITE_AWS_REGION,
-//       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-//       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-//     },
-//   },
-// });
-
 const MainLayout = ({ children, user }) => (
   <>
     <Navbar user={user} />
